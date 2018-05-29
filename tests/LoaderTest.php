ebacdd7f27a4b6e6fb308cfd68ab1c4d2cbdee7b
Bradford Condon <bradford.condon@gmail.com>
2018-05-29 18:14:05 -0400

    add laceys test file that almost works

--- tests/LoaderTest.php
+++ tests/LoaderTest.php
@@ -8,7 +8,18 @@ class LoaderTest extends TripalTestCase {
    use DBTransaction;
 
 
-  public function testLoaderFunctionality(){
+  public function testLaceyFile(){
+
+
+    tripal_bibtex_import_bibtex('example/no_ws_example.bib');
+
+    $query=  db_select('chado.pub', 'p')->fields('p', ['title', 'type_id'])->condition('title', 'Tripal v0000: a test bibtex file');//title from the example file
+    $result = $query->execute()->FetchAll();
+
+    $this->assertNotEmpty($result);
+
+  }
+  public function testLoaderFunctionalityWhiteSpace(){
 
     tripal_bibtex_import_bibtex('example/example.bib');
 
