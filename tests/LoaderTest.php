12bd615cb12bb082e1b19402724df0808d49b87d
Bradford Condon <bradford.condon@gmail.com>
2018-05-29 18:41:17 -0400

    laceys file passes tests

--- tests/LoaderTest.php
+++ tests/LoaderTest.php
@@ -5,23 +5,20 @@ use StatonLab\TripalTestSuite\DBTransaction;
 use StatonLab\TripalTestSuite\TripalTestCase;
 
 class LoaderTest extends TripalTestCase {
-   use DBTransaction;
+  use DBTransaction;
 
-
-  public function testLaceyFile(){
-
+  public function testLoadFOneThousandStyleBibTex(){
 
     tripal_bibtex_import_bibtex('example/no_ws_example.bib');
 
-    $query=  db_select('chado.pub', 'p')->fields('p', ['title', 'type_id'])->condition('title', 'Tripal v0000: a test bibtex file');//title from the example file
+    $query=  db_select('chado.pub', 'p')->fields('p', ['title', 'type_id'])->condition('title', 'NO WHITE SPACE');//title from the example file
     $result = $query->execute()->FetchAll();
-
     $this->assertNotEmpty($result);
 
   }
   public function testLoaderFunctionalityWhiteSpace(){
 
-    tripal_bibtex_import_bibtex('example/example.bib');
+  //  tripal_bibtex_import_bibtex('example/example.bib');
 
   $query=  db_select('chado.pub', 'p')->fields('p', ['title', 'type_id'])->condition('title', 'Tripal v0000: a test bibtex file');//title from the example file
   $result = $query->execute()->FetchAll();
