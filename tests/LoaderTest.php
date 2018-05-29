c35e77a8895bcd20766c3570a9a9ef29ed210506
Bradford Condon <bradford.condon@gmail.com>
2018-05-29 17:40:45 -0400

    init unit test and struggle with getting example loader working

---
+++ tests/LoaderTest.php
@@ -0,0 +1,24 @@
+<?php
+namespace Tests;
+
+use StatonLab\TripalTestSuite\DBTransaction;
+use StatonLab\TripalTestSuite\TripalTestCase;
+
+class LoaderTest extends TripalTestCase {
+   use DBTransaction;
+
+
+  public function testLoaderFunctionality(){
+
+    tripal_bibtex_import_bibtex('example/example.bib');
+
+  $query=  db_select('chado.pub', 'p')->fields('p', ['title', 'type_id'])->condition('title', 'Tripal v0000: a test bibtex file');//title from the example file
+  $result = $query->execute()->FetchAll();
+
+//    $query=  db_select('chado.pub', 'p')->fields('p');//title from the example file
+//    $result = $query->execute()->FetchAll();
+//
+  $this->assertNotEmpty($result);
+  }
+
+}
